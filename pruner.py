7e15c6da80975c87017ee4feb5baeba6270db84d
Ayedux <aidanjw05@gmail.com>
2020-11-13 14:15:51 +1300

    Added docstrings

--- pruner.py
+++ pruner.py
@@ -3,6 +3,15 @@ import sys
 import sentence
 
 def pgn_getter():
+    """
+    A function which prompts the user to either paste a pgn as text or to enter the directory of a pgn file.\n
+
+    The user first selects whether to paste the pgn as text or to enter the directory location of the file.
+    The user is then prompted to either enter the text or directory location.
+     - If they enter a directory, it is passed through a the remove_spaces function to remove all spaces from the directory name.
+      - The file is then retrieved and the pgn converted into plain text where it is stored under a variable.
+     - If the enter a pgn as text, it is stored under a variable.
+    """
     while True:
         path = int(input("Press 1 to past a pgn as plain text or press 2 to enter an address to a pgn\n"))
         if path == 1:
@@ -10,21 +19,31 @@ def pgn_getter():
             break
         elif path == 2:
             pgn_location = input("Enter location your pgn:\n")
-            #removes space from directory name
+            #Removes space from directory name
             remove_spaces(pgn_location)
             print(os.path.isfile(pgn_location))
 
             if os.path.isfile(pgn_location):
                 print(os.listdir(pgn_location))
                 break
-    
+  
 def move_to_prune():
+    '''
+    A function that prunes a pgn to a user defined number of moves.\n
+
+    Will find the longest branch in the pgn and compare it to the user defined length they wish to prune to.
+    If the lognest branch is shorter than the user difined value, they are asked to enter a smaller number.
+    Provided that the user input is positive, non 0 and an iteger, the program will then continue.
+    '''
+
     max_moves = input("How many moves would you like to prune too? ")
 
-"""
-Used to remove all white space in the directory name just incase there is a space or spaces in the give directory
-"""
-def remove_spaces(dir):
+def remove_spaces(dir: str) -> str:
+    """
+    A simple function which removes all spaces in a give directory.\n
+
+    This is so the directory name given is accurate and can be found by the program.
+    """
     sentence.strip(dir)
 
 def main():
