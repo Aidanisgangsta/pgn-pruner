06fd4ae42e5c9f89e998991342a6dc17f09b9be5
Ayedux <aidanjw05@gmail.com>
2020-11-13 13:21:33 +1300

    Added docstrings and functions

--- pruner.py
+++ pruner.py
@@ -1,17 +1,34 @@
 import os 
 import sys
+import sentence
 
-while True:
-    path = int(input("Press 1 to past a pgn as plain text or press 2 to enter an address to a pgn\n"))
-    if path == 1:
-        pgn_text = input("Please past pgn here:\n")
-        break
-    elif path == 2:
-        pgn_location = input("Enter location your pgn:\n")
-        if os.path.exists(pgn_location):
-            print(os.listdir(pgn_location))
+def pgn_getter():
+    while True:
+        path = int(input("Press 1 to past a pgn as plain text or press 2 to enter an address to a pgn\n"))
+        if path == 1:
+            pgn_text = input("Please past pgn here:\n")
             break
+        elif path == 2:
+            pgn_location = input("Enter location your pgn:\n")
+            #removes space from directory name
+            remove_spaces(pgn_location)
+            print(os.path.isfile(pgn_location))
+
+            if os.path.isfile(pgn_location):
+                print(os.listdir(pgn_location))
+                break
     
+def move_to_prune():
+    max_moves = input("How many moves would you like to prune too? ")
 
+"""
+Used to remove all white space in the directory name just incase there is a space or spaces in the give directory
+"""
+def remove_spaces(dir):
+    sentence.strip(dir)
 
-max_moves = input("How many moves would you like to prune too? ")
+def main():
+    pgn_getter()
+    move_to_prune()
+
+main()
