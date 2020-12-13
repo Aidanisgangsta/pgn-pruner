e474595af5dc906a3778c2c089c3df64c33d9973
Ayedux <aidanjw05@gmail.com>
2020-12-14 02:59:07 +1300

    Created function to convert brackets into a list

--- brackettuple.py
+++ brackettuple.py
@@ -2,3 +2,14 @@ e1 = "( ( ) ( ) ) ( ( ) ( ( ) ) ) ( )"
 e2 = "(()(())())"
 e3 = "( ( ( ) ( ( ) ( ) ) ) ( ( ) ( ) ) )"
 
+def bracket_pairs(ex):
+    """
+    A function that will find the pairs of brackets in the pgn.\n
+
+    -Returns a list of tuples containing the pairs of array locations of the bracket pairs.
+    """
+
+    lst = []
+
+    for bracket in ex:
+        lst.append(bracket)
